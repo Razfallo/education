ebeb3332feebf1000db60de0b0650de9cf47b10c
Razfallo <46151568+Razfallo@users.noreply.github.com>
2021-02-02 10:50:58 +0300

    1

--- mob_pay.py
+++ mob_pay.py
@@ -1,8 +1,6 @@
 import card_balance
 import cards_list
 
-# 222
-
 
 def mob_payment():
     phrase = input("Введите сумму и карту: ")
@@ -11,46 +9,45 @@ def mob_payment():
     i = 0
     digit_check = 0
     card_check = 0
+    final_phrase = {"final_amount": "final_amount", "final_card": "final_card"}
     if len(phrase_by_words) < 1:
         print("Введены некорректные данные.\n")
         return None
     while i < len(phrase_by_words):
         if phrase_by_words[i].replace('.', '', 1).isdigit():
-            phrase_by_words[i] = amount_check(phrase_by_words[i])
-            phrase_by_words.insert(0, phrase_by_words[i])
+            final_phrase["final_amount"] = phrase_by_words[i]
             print("Введена сумма:", phrase_by_words[i])
             digit_check += 1
-            i += 1
         elif phrase_by_words[i] == "stop":
-            i = len(phrase_by_words)
             stop()
+            break
         elif str(phrase_by_words[i]) in cards_list.cards():
-            phrase_by_words.insert(1, phrase_by_words[i])
+            final_phrase["final_card"] = phrase_by_words[i]
             print("Выбрана карта:", phrase_by_words[i])
-            i += 1
             card_check += 1
         i += 1
     print(phrase_by_words)
+    print(final_phrase)
     print("digits:", digit_check, "\n", "cards", card_check)  # проверка перебора списка
     if digit_check == 1 and card_check == 1:
-        print("Перевожу", phrase_by_words[0], "c", phrase_by_words[1])
+        print("Перевожу", final_phrase["final_amount"], "c", final_phrase["final_card"])
         return None
     if digit_check > 1 or card_check > 1:
         print("Введены некорректные данные")
         return None
     elif digit_check == 0 and card_check == 1:
-        amount = amount_check(input("Введите сумму: "))
+        amount = amount_inbalance(input("Введите сумму: "))
         if amount == "stop":
             stop()
         else:
             print("Перевожу", amount, "c", phrase_by_words[0])
             return None
     elif digit_check == 1 and card_check == 0:
-        card = card_choice(input("Выберите карту: "))
+        card = card_isknown(input("Выберите карту: "))
         if card == "stop":
             stop()
         else:
-            print("Перевожу", phrase_by_words[0], "c", card)
+            print("Перевожу", amount_inbalance(phrase_by_words[0]), "c", card)
             return None
 
 
@@ -59,7 +56,7 @@ def stop():
     return None
 
 
-def amount_check(amount):
+def amount_inbalance(amount):
     balance = card_balance.balance()
     try:
         amount = round(float(amount), 2)
@@ -67,25 +64,25 @@ def amount_check(amount):
             return amount
         elif amount > balance:
             print("Недостаточно средств")
-            return amount_check(input("Повторите ввод суммы: "))
+            return amount_inbalance(input("Повторите ввод суммы: "))
         else:
             print("Введены некорректные данные.\n")
-            return amount_check(input("Повторите ввод суммы: "))
+            return amount_inbalance(input("Повторите ввод суммы: "))
     except ValueError:
         if str(amount) == "stop":
             return "stop"
         else:
-            return amount_check(input("Повторите ввод суммы: "))
+            return amount_inbalance(input("Повторите ввод суммы: "))
 
 
-def card_choice(voice_in):
+def card_isknown(voice_in):
     if voice_in in cards_list.cards():
         card = voice_in
         return card
     elif voice_in == "stop":
         return "stop"
     else:
-        return card_choice(input("Повторите ввод карты: "))
+        return card_isknown(input("Повторите ввод карты: "))
 
 
 if __name__ == "__main__":  # Переменная __name__ указывает на имя модуля. Для главного модуля, который непосредственно
