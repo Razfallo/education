2b47a7755603a129c4a22dbd0e0686876844b29c
Razfallo <46151568+Razfallo@users.noreply.github.com>
2021-02-02 20:31:07 +0300

    prya

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-companies = ["Microsoft", "Google", "Oracle", "Apple"]
-for item in companies:
-    print(item)
+import mob_pay
+
+
+def test_common_request():
+    request = "100.123 visa"
+    mob_pay.input = lambda x: request
+    """
+    переменная Input вызывает не встроенную ф-ию, а новую определенную нами. 
+    В данном случае lambd'y, которая  возврашает переменная request
+    """
+    mob_pay.mob_payment()
+
+
+test_common_request()
