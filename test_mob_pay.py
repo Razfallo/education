68664078fab235b4d01c2525e75444e871fbd425
Razfallo <46151568+Razfallo@users.noreply.github.com>
2021-02-14 15:28:38 +0300

    подправил тесты

--- test_mob_pay.py
+++ test_mob_pay.py
@@ -10,8 +10,9 @@ def check_result(result: str):
 
 def test_common_request():
     print("______________________________\nСтандартный ввод\n______________________________")
-    request = "100.123 visa"
-    mob_pay.input = lambda x: request
+    step_inputs = {0: "100.123 visa mom", 1: "да"}
+    context = {"step_inputs": step_inputs, "step_number": 0}
+    mob_pay.input = lambda x: input_steps(context)
     """
     переменная Input вызывает не встроенную ф-ию, а новую определенную нами. 
     В данном случае lambd'y, которая  возврашает переменная request
@@ -21,39 +22,52 @@ def test_common_request():
 
 def test_common_request_2():
     print("______________________________\nСтандартный ввод №2\n______________________________")
-    mob_pay.input = lambda x: "mastercard 123"
+    step_inputs = {0: "bro mastercard 123", 1: "да"}
+    context = {"step_inputs": step_inputs, "step_number": 0}
+    mob_pay.input = lambda x: input_steps(context)
     check_result(mob_pay.mob_payment())
 
 
 def test_with_no_input():
     print("______________________________\nПустой ввод\n______________________________")
-    mob_pay.input = lambda x: " "
+    step_inputs = {0: " ", 1: "да"}
+    context = {"step_inputs": step_inputs, "step_number": 0}
+    mob_pay.input = lambda x: input_steps(context)
     check_result(mob_pay.mob_payment())
-
 
 def test_with_rubbish():
     print("______________________________\nВвод с <мусором>\n______________________________")
-    mob_pay.input = lambda x: "mastercard 123 rubbish"
+    step_inputs = {0: "mastercard 123 rubbish mom", 1: "да"}
+    context = {"step_inputs": step_inputs, "step_number": 0}
+    mob_pay.input = lambda x: input_steps(context)
     check_result(mob_pay.mob_payment())
 
 
 def test_doubled_amount():
     print("______________________________\nВвод с 2мя суммами\n______________________________")
-    step_inputs = {0: "123 123 visa", 1: "321 visa"}
+    step_inputs = {0: "123 1213 visa bro", 1: "да"}
     context = {"step_inputs": step_inputs, "step_number": 0}
-    mob_pay.input = lambda x: _on_doubled_input(context)
+    mob_pay.input = lambda x: input_steps(context)
     check_result(mob_pay.mob_payment())
 
 
 def test_doubled_card():
     print("______________________________\nВвод с 2мя картами\n______________________________")
-    step_inputs = {0: "123 mastercard visa", 1: "321 visa"}
+    step_inputs = {0: "123 mastercard visa", 1: "да"}
     context = {"step_inputs": step_inputs, "step_number": 0}
-    mob_pay.input = lambda x: _on_doubled_input(context)
+    mob_pay.input = lambda x: input_steps(context)
     check_result(mob_pay.mob_payment())
 
 
-def _on_doubled_input(context: dict) -> str:
+# def test_doubled_amount():
+#     print("______________________________\nВвод с 2мя суммами\n______________________________")
+#     step_inputs = {0: "123 123 visa bro", 1: "321 visa"}
+#     context = {"step_inputs": step_inputs, "step_number": 0}
+#     mob_pay.input = lambda x: _on_doubled_input(context)
+#     check_result(mob_pay.mob_payment())
+
+
+def input_steps(context: dict) -> str:
     step_number = context["step_number"]
     if step_number > 2:
         raise ValueError(step_number)
@@ -63,9 +77,13 @@ def _on_doubled_input(context: dict) -> str:
     return result
 
 
+
+
+
 if __name__ == "__main__":
     test_common_request()
     test_common_request_2()
+    test_with_no_input()
     test_with_rubbish()
     test_doubled_amount()
     test_doubled_card()
